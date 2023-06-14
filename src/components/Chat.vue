ede18a03fb12fc5f9d05105555bdaa64f5d02307
paxasonik <paxasonk@mail.ru>
2023-06-14 15:54:16 +0300

    added chat component and styles

---
+++ src/components/Chat.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container content__chat chat">
+    <ul class="chat__messages message">
+      <li class="message__item user">
+        Message
+      </li>
+      <li class="message__item my">
+        My message
+      </li>
+    </ul>
+    <div class="chat__add-massage">
+      <input class="input" type="text" placeholder="Введите сообщение">
+      <button class="btn">
+        Отправить
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
